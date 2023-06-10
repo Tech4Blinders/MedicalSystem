8fd2db6a0c5c36f66e087d4efa03dd0fc3db6c36
Karim Ali <kariimalii96@gmail.com>
2023-06-10 14:26:14 +0200

    Added Image Upload Feature

--- Api/MedicalSystem.Api/Controllers/DoctorController.cs
+++ Api/MedicalSystem.Api/Controllers/DoctorController.cs
@@ -2,6 +2,9 @@
 using MedicalSystem.CoreLayer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 
 namespace MedicalSystem.Api.Controllers
 {
@@ -10,10 +13,12 @@ namespace MedicalSystem.Api.Controllers
     public class DoctorController : ControllerBase
     {
         private readonly IDoctorManager _doctorManager;
+        private readonly IConfiguration _config;
 
-        public DoctorController(IDoctorManager doctorManager)
+        public DoctorController(IDoctorManager doctorManager , IConfiguration config)
         {
             _doctorManager = doctorManager;
+            _config = config;
         }
         [HttpGet]
         public  ActionResult<IEnumerable<ReadDoctorDto>> GetAll() =>  Ok(_doctorManager.GetAll());
@@ -26,9 +31,31 @@ namespace MedicalSystem.Api.Controllers
             return Ok(doctor);
         }
         [HttpPost]
-        public  ActionResult<ReadDoctorDto> Add(AddDoctorDto doctorDto)
+        public async Task<ActionResult<ReadDoctorDto>>  Add([FromForm] AddDoctorDto doctorDto)
         {
-            return Ok(_doctorManager.Add(doctorDto));
+            if (doctorDto.File == null || doctorDto.File.Length == 0)
+            {
+                return BadRequest("No file was uploaded.");
+            }
+            var cloudinaryCloudName = _config.GetValue<string>("cloudinaryCloudName");
+            var cloudinaryApiKey = _config.GetValue<string>("cloudinaryApiKey");
+            var cloudinaryApiSecret = _config.GetValue<string>("cloudinaryApiSecret");
+            //var cloudinaryURL = _config.GetValue<string>("CLOUDINARY_URL");
+            Account account = new Account (cloudinaryCloudName, cloudinaryApiKey, cloudinaryApiSecret);
+            Cloudinary cloudinary = new Cloudinary(account);
+            cloudinary.Api.Secure = true;
+            string fileName = doctorDto.File.FileName;
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(fileName, doctorDto.File.OpenReadStream()),
+                UseFilename = true,
+                UniqueFilename = false,
+                Overwrite = true,
+                Folder="Medical System"
+            };
+            var uploadResult = cloudinary.Upload(uploadParams);
+            doctorDto.Image = uploadResult.SecureUrl.ToString();
+            return await _doctorManager.Add(doctorDto);
         }
         [HttpDelete("{id}")]
         public ActionResult Delete(int id)

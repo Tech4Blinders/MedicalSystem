d2a995b08c6ca62bc931190dd4137beb0f38a945
hesham abden <heshamabden29@gmail.com>
2023-06-17 22:11:21 +0300

    Appointment Component and Service

--- Client/src/app/hospital/hospital.module.ts
+++ Client/src/app/hospital/hospital.module.ts
@@ -8,10 +8,11 @@ import { HospitalRoutingModule } from './hospital-routing.module';
 import { Hosection1Component } from './hosection1/hosection1.component';
 import { Section6Component } from './section6/section6.component';
 import { CardSectionComponent } from './card-section/card-section.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
-  declarations: [HospitalComponent, HoSection2Component, Hosection1Component, Section6Component, CardSectionComponent,],
+  declarations: [HospitalComponent, HoSection2Component, Hosection1Component, Section6Component, CardSectionComponent, AppointmentComponent,],
   imports: [CommonModule, HospitalRoutingModule,CardModule],
-  exports: [HoSection2Component, Hosection1Component, Section6Component,],
+  exports: [HoSection2Component, Hosection1Component, Section6Component,AppointmentComponent],
 })
 export class HospitalModule {}

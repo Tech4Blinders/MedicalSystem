989f4631ba0985b7bca1cf4b7e1fd30c04a7d4b0
miadasaad <miadasaad345@gmail.com>
2023-06-21 22:24:21 +0300

    update homeGallery

--- Client/src/app/home/section1/section1.component.ts
+++ Client/src/app/home/section1/section1.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HospitalService } from 'src/app/Services/hospital.service';
 import { PhotoService } from 'src/app/Services/photo.service';
 
 @Component({
@@ -6,11 +8,27 @@ import { PhotoService } from 'src/app/Services/photo.service';
   templateUrl: './section1.component.html',
   styleUrls: ['./section1.component.css'],
 })
-export class Section1Component {
+export class Section1Component implements OnInit{
   id: string = '1';
+  hospitals:any[];
+  route:ActivatedRoute;
+  constructor(private Photo: PhotoService, private hospitalService: HospitalService,private router:Router) {}
 
-  constructor(private Photo: PhotoService) {}
+  ngOnInit(): void {
+    this.getHospitals();
+  }
 
   images: any[] = this.Photo.getPhotos();
   responsiveOptions: any[] = this.Photo.getResponsiveOptions();
+  getHospitals(){
+    this.hospitalService.getAllHospitals().subscribe((a:any) => {
+      this.hospitals = a;
+      console.log(a);
+      
+    })
+  }
+  setHospital(item:any){
+    this.hospitalService.setHospital(item);
+    this.router.navigate(["visit"],{relativeTo:this.route})
+  }
 }
